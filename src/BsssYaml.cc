4e150dbc3b0ed85d705fe59a7f88ec1e01671ea1
Kukhee Kim <khkim@slac.stanford.edu>
2022-12-08 14:05:43 -0800

    add a flag to recognie BSSS calls the service, remove rate control related registers

--- src/BsssYaml.cc
+++ src/BsssYaml.cc
@@ -4,16 +4,13 @@
 using namespace Bsss;
 
 BsssYaml::BsssYaml(Path bsss_path)
-:AcqServiceYaml(bsss_path, NUM_BSSS_CHN)
+:AcqServiceYaml(bsss_path, NUM_BSSS_CHN, AcqService::bsss)
 {
-    for(int i = 0; i < _edef_num; i++) {
-        char str_name[80];
+    int i = 0;
+    char str_name[80];
 
-        sprintf(str_name, "EdefEnable[%d]",    i); _EdefEnable[i]    = IScalVal::create(_path->findByName(str_name));
-        sprintf(str_name, "EdefRateLimit[%d]", i); _EdefRateLimit[i] = IScalVal::create(_path->findByName(str_name));
-        sprintf(str_name, "EdefRateSel[%d]",   i); _EdefRateSel[i]   = IScalVal::create(_path->findByName(str_name));
-        sprintf(str_name, "EdefDestSel[%d]",   i); _EdefDestSel[i]   = IScalVal::create(_path->findByName(str_name));
-    }
+    sprintf(str_name, "EdefRateLimit[%d]", i); _EdefRateLimit[i] = IScalVal::create(_path->findByName(str_name));
+
 }
 
 
