a44bfbb8b971769bb9795f42914dfee106a09642
Kukhee Kim <khkim@slac.stanford.edu>
2022-12-09 13:25:22 -0800

    get number of EDEFs from uppper layer softare, since each BSSS module covers different number of EDEFs

--- src/BsssYaml.cc
+++ src/BsssYaml.cc
@@ -3,8 +3,8 @@
 
 using namespace Bsss;
 
-BsssYaml::BsssYaml(Path bsss_path)
-:AcqServiceYaml(bsss_path, NUM_BSSS_CHN, AcqService::bsss)
+BsssYaml::BsssYaml(Path bsss_path, uint32_t num_edef)
+:AcqServiceYaml(bsss_path, num_edef, AcqService::bsss)
 {
     int i = 0;
     char str_name[80];
